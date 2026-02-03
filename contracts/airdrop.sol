cd60ff2da3767e7e6e5e225e9183325a22747244
chreamy <chrislinkou@gmail.com>
2026-02-04 03:50:25 +0800

    airdrop update

--- contracts/airdrop.sol
+++ contracts/airdrop.sol
@@ -2,35 +2,29 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Airdrop is Ownable(msg.sender) {
+contract Airdrop {
+
     event Airdropped(address indexed to, uint256 amount);
 
     function airdrop(
         address token,
-        address from,
         address[] calldata recipients,
         uint256[] calldata amounts
-    ) external onlyOwner {
+    ) external {
         require(recipients.length == amounts.length, "Length mismatch");
 
         IERC20 erc20 = IERC20(token);
+
         uint256 total;
         for (uint256 i = 0; i < amounts.length; i++) {
             total += amounts[i];
         }
 
-        require(
-            erc20.transferFrom(from, address(this), total),
-            "TransferFrom failed"
-        );
+        erc20.transferFrom(msg.sender, address(this), total);
 
         for (uint256 i = 0; i < recipients.length; i++) {
-            require(
-                erc20.transfer(recipients[i], amounts[i]),
-                "Transfer failed"
-            );
+            erc20.transfer(recipients[i], amounts[i]);
             emit Airdropped(recipients[i], amounts[i]);
         }
     }
